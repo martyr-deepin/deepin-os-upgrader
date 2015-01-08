56bebdf4c66e266172fd731bf3e1cff19de2745b
xufasheng <fasheng.xu@gmail.com>
2015-01-08 16:21:59 +0800

    add update_grub
    tower: https://tower.im/projects/3b99b1824fd74228be438acd3189fb7b/todos/17ab886cf63446c682e0143de43598b5/#a702e07f03c445d1b00f961fce2c7e09
    Change-Id: I629b8c34f37f7220aedb498fb4526625357fb555

--- from2014.1to2014.2.sh
+++ from2014.1to2014.2.sh
@@ -5,3 +5,5 @@ apt-get purge tlp
 dpkg -l | grep kingsoft-office >/dev/null && apt-get purge -y kingsoft-office && apt-get install wps-office
 dpkg -l | grep wps-office- >/dev/null && apt-get purge  wps-office-* && apt-get install wps-office
 apt-get update && apt-get dist-upgrade
+/usr/lib/deepin-daemon/grub2 --setup
+update-grub
--- from2014.2RCto2014.2.sh
+++ from2014.2RCto2014.2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm -rf /usr/share/icons/*/icon-theme.cache
 apt-get update && apt-get dist-upgrade
+/usr/lib/deepin-daemon/grub2 --setup
+update-grub
